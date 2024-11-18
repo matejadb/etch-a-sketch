b6304af98f6d5f1a041093042fae0e790478b9b9
matejadb <92729211+matejadb@users.noreply.github.com>
2024-11-18 18:10:26 +0100

    Add hover and random rgb colors
    Divs change color on hover, the colors are random rgb values

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const container = document.querySelector(".container");
 
 for (let i = 1; i < 273; i++) {
 	let square = document.createElement("div");
-
 	square.classList.add("box");
+
+	square.onmouseover = () => {
+		let r = Math.floor(Math.random() * 256) + 1;
+		let g = Math.floor(Math.random() * 256) + 1;
+		let b = Math.floor(Math.random() * 256) + 1;
+		square.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+	};
+
 	container.appendChild(square);
 }
