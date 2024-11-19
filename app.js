c53662e68e44ff775ba4041dfa73cf2affd95798
matejadb <92729211+matejadb@users.noreply.github.com>
2024-11-19 18:51:31 +0100

    test

--- app.js
+++ app.js
@@ -31,17 +31,5 @@ popupButton.addEventListener("click", (e) => {
 		}
 	}
 });
-/*
-function createBoxes(numBox) {
-	for (let i = 0; i < numBox; i++) {
-		const row = container.appendChild(document.createElement("div"));
-		for (let j = 0; j < numBox; j++) {
-			const square = document.createElement("div");
-			square.className = "box";
-			row.appendChild(square);
-		}
-	}
-}
 
-createBoxes(16);
-*/
+// TODO - Fix grid
